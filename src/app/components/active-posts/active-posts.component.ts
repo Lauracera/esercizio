0b3a5e405eaa5ef10add4937d13abf055738423f
Laura Ceragioli <141077304+Lauracera@users.noreply.github.com>
2023-11-29 16:48:17 +0100

    first commit

---
+++ src/app/components/active-posts/active-posts.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit, Input } from '@angular/core';
+import { Post } from 'src/app/models/post';
+import { PostsService } from 'src/app/service/posts.service';
+
+@Component({
+    selector: 'app-active-posts',
+    templateUrl: './active-posts.component.html',
+    styleUrls: ['./active-posts.component.scss'],
+})
+export class ActivePostsComponent implements OnInit {
+    posts: Post[] = [];
+
+    constructor(private postSrv: PostsService) {
+        this.postSrv.recuperaPosts().then((posts) => {
+            this.posts = posts;
+        });
+    }
+
+    @Input() post: any;
+
+    ngOnInit(): void {}
+}
